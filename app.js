3ed729337615f7ea24e5098877ed3a12b6fcc239
ilhov.space_hp <ilhov.space@gmail.com>
2020-12-16 12:07:54 +0300

    Creating routes

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const authRoutes = require('./routes/auth')
+const analyticsRoutes = require('./routes/analytics')
+const positionRoutes = require('./routes/position')
+const orderRoutes = require('./routes/order')
+const categoryRoutes = require('./routes/category')
 const app = express()
 
 app.use('/api/auth', authRoutes)
+app.use('/api/analytics', analyticsRoutes)
+app.use('/api/position', positionRoutes)
+app.use('/api/order', orderRoutes)
+app.use('/api/category', categoryRoutes)
 
 module.exports = app
