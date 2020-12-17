b9e9073aac7caea74e26b46ce7bac4dfaaa3f560
ilhov.space_hp <ilhov.space@gmail.com>
2020-12-17 22:22:16 +0300

    register new user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs')
+const User = require('../models/User')
+
 module.exports.login = function(req, res){
     res.status(200).json({
         login: {
@@ -7,8 +10,26 @@ module.exports.login = function(req, res){
     })
 }
 
-module.exports.register = function(req, res){
-    res.status(200).json({
-        register: 'log from cont'
-    })
+module.exports.register = async function(req, res){
+     const candidate = await User.findOne({email: req.body.password})
+     if(candidate){
+         res.status(409).json({
+             massage: 'Пользователь с таким Email уже зарегистрирован'
+         })
+     } else {
+        const solt = bcrypt.genSaltSync(12)
+        const pass = req.body.password
+
+        const user = new User({
+            email: req.body.email,
+            password: bcrypt.hashSync(pass, solt)
+        })
+        try {
+            await user.save()
+            res.status(201).json(user)
+        } catch(e){
+            console.log(e)
+        }
+     }
+
 }
