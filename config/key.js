2a87148145f78f327fee59a75e7a8b5f9dcb2266
ilhov.space_hp <ilhov.space@gmail.com>
2020-12-17 22:59:37 +0300

    login user

--- config/key.js
+++ config/key.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://vlad:123moon@cluster0.ytvyq.mongodb.net/Cluster0?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://vlad:123moon@cluster0.ytvyq.mongodb.net/Cluster0?retryWrites=true&w=majority',
+    jwt: 'dev-token'
 }
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,44 @@
 const bcrypt = require('bcryptjs')
 const User = require('../models/User')
+const jwt = require('jsonwebtoken')
+const keys = require('../config/key')
 
-module.exports.login = function(req, res){
-    res.status(200).json({
-        login: {
-            email: req.body.email,
-            password: req.body.password 
-        } 
-    })
+
+// res.status(200).json({
+//     login: {
+//         email: req.body.email,
+//         password: req.body.password 
+//     } 
+// })
+
+module.exports.login = async function(req, res){
+    const candidate = await User.findOne({email: req.body.email})
+    if(candidate){
+        const pass = bcrypt.compareSync(req.body.password, candidate.password)
+        if(pass){
+            const token = jwt.sign({
+                userId: candidate._id,
+                email: candidate.email
+            }, keys.jwt, {expiresIn: 3600})
+
+            res.status(200).json({
+                token: `Bearer ${token}`
+            })
+        } else {
+            res.status(401).json({
+                massage: 'Введен неверный логин или пароль'
+            })
+        }
+    } else {
+        res.status(401).json({
+            massage: 'Введен неверный логин или пароль'
+        })
+    }
+
 }
 
 module.exports.register = async function(req, res){
-     const candidate = await User.findOne({email: req.body.password})
+     const candidate = await User.findOne({email: req.body.email})
      if(candidate){
          res.status(409).json({
              massage: 'Пользователь с таким Email уже зарегистрирован'
