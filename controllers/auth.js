b13d4d2507baae20d49cebdd673555956c9eb6ba
ilhov.space_hp <ilhov.space@gmail.com>
2020-12-18 09:40:17 +0300

    added passport js

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,14 +3,6 @@ const User = require('../models/User')
 const jwt = require('jsonwebtoken')
 const keys = require('../config/key')
 
-
-// res.status(200).json({
-//     login: {
-//         email: req.body.email,
-//         password: req.body.password 
-//     } 
-// })
-
 module.exports.login = async function(req, res){
     const candidate = await User.findOne({email: req.body.email})
     if(candidate){
